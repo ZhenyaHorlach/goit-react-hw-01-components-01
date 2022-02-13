192a4c0a3ba0cf09d86fe4865239ca583be56c37
Zhenya Horlach <gorlachjob@ukr.net>
2022-02-13 07:43:31 +0200

    add new chenges and delete 2 files

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import s from './Statistics.module.css'
 
-export default function Statistics({ stats }) {
+export default function Statistics({ title, stats }) {
     return (
         <section className={s.statistics}>
-            <h2 className={s.title}>Upload stats</h2>
+            {title && <h2 className={s.title}>{title}</h2>}
 
             <ul className={s.statList}>
                 {stats.map(stat => (
